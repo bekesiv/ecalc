a8eb15afd12b5001ebb77b072e4efd52586c1b79
Viktor Bekesi <viktor.bekesi@outlook.com>
2023-11-24 00:19:37 +0100

    All basic funcionalities are working

--- ecalc/calculator.py
+++ ecalc/calculator.py
@@ -3,43 +3,78 @@
 import customtkinter as ctk
 import ast
 from math import *
-import re
 
-class Calculator(object):
+class Calculator(ctk.CTk):
     def __init__(self):
+        super().__init__()
         ctk.set_appearance_mode('Dark')
         ctk.set_default_color_theme('blue')
-        self.app = ctk.CTk()
-        self.app.title('eCalc')
-        self.app.geometry('800x400')
-        self.app.resizable(False, False)
-
-        self.bInput = ctk.CTkEntry(master=self.app, width=800, font=('Calibry', 20), placeholder_text='Type Expression Here', justify='right')
+        # Main Window
+        self.title('eCalc')
+        self.geometry('800x400')
+        self.minsize(600, 300)
+        self.grid_rowconfigure((0, 1, 2, 4), weight=0)
+        self.grid_rowconfigure((3), weight=1)
+        self.grid_columnconfigure(0, weight=2)
+        self.grid_columnconfigure((1, 2), weight=1)
+        self.bind('<Escape>', lambda e, w=self: w.destroy())
+        # Input TextBox
+        self.bInput = ctk.CTkEntry(master=self, width=800, font=('Calibry', 20), placeholder_text='Type Expression Here', justify='right')
         self.bInput.bind('<KeyRelease>', self.onChangeInput)
-        self.bInput.pack(pady=20, padx=20)
-        self.bInput.after(20, self.bInput.focus_set)
+        self.bInput.grid(row=0, column=0, columnspan=3, ipadx=8, ipady=4, padx=8, pady=4, sticky="ew")
+        self.bInput.after(30, self.bInput.focus_set)
         self.bInput.focus_set()
-
-        self.lResultDec = ctk.CTkLabel(master=self.app, text='Result', font=('Calibry', 16))
-        self.lResultDec.pack(pady=10, padx=20)
-
-        self.bResultDec = ctk.CTkEntry(master=self.app, width=400, font=('Calibry', 20), state=ctk.DISABLED)
+        # Below controls are organized into a frame
+        self.frame = ctk.CTkFrame(master=self)
+        self.frame.grid(row=1, column=0, columnspan=3, padx=8, pady=4, sticky="ew")
+        self.frame.grid_columnconfigure(1, weight=1)
+        # Decimal Result Caption
+        self.lResultDec = ctk.CTkLabel(master=self.frame, text='Result', font=('Calibry', 16), anchor=ctk.W)
+        self.lResultDec.grid(row=0, column=0, padx=8, pady=4, sticky="ew")
+        # Decimal TextBox
+        self.bResultDec = ctk.CTkEntry(master=self.frame, width=400, font=('Calibry', 20), justify='right', state=ctk.DISABLED)
         self.bResultDec.bind('<Button>', self.onClickResultDec)
-        self.bResultDec.pack(pady=10, padx=20)
-
-        self.lResultHex = ctk.CTkLabel(master=self.app, text='Hexadecimal', font=('Calibry', 16))
-        self.lResultHex.pack(pady=10, padx=20)
-
-        self.bResultHex = ctk.CTkEntry(master=self.app, width=400, font=('Calibry', 20), state=ctk.DISABLED)
-        self.bResultHex.pack(pady=10, padx=20)
+        self.bResultDec.grid(row=0, column=1, padx=0, pady=4, sticky="ew")
+        # Hexadecimal Result Caption
+        self.lResultHex = ctk.CTkLabel(master=self.frame, text='Hexadecimal', font=('Calibry', 16), anchor=ctk.W)
+        self.lResultHex.grid(row=1, column=0, padx=8, pady=4, sticky="ew")
+        # Hexadecimal TextBox
+        self.bResultHex = ctk.CTkEntry(master=self.frame, width=400, font=('Calibry', 20), justify='right', state=ctk.DISABLED)
+        self.bResultHex.grid(row=1, column=1, padx=0, pady=4, sticky="ew")
+        # Expression History Caption
+        self.lExpressionHistory = ctk.CTkLabel(master=self, text='Expression History', font=('Calibry', 16), anchor=ctk.W)
+        self.lExpressionHistory.grid(row=2, column=0, padx=16, pady=4, sticky="ew")
+        # Decimal History Caption
+        self.lHistoryDec = ctk.CTkLabel(master=self, text='Decimal History', font=('Calibry', 16), anchor=ctk.W)
+        self.lHistoryDec.grid(row=2, column=1, padx=16, pady=4, sticky="ew")
+        # Hexadecimal History Caption
+        self.lHistoryHex = ctk.CTkLabel(master=self, text='Hexadecimal History', font=('Calibry', 16), anchor=ctk.W)
+        self.lHistoryHex.grid(row=2, column=2, padx=16, pady=4, sticky="ew")
+        # Expression History
+        self.bExpressionHistory = ctk.CTkTextbox(master=self, font=('Calibry', 16), spacing1=2, spacing3=2)
+        self.bExpressionHistory.grid(row=3, column=0, padx=8, pady=0, sticky="nsew")
+        # Decimal History
+        self.bHistoryDec = ctk.CTkTextbox(master=self, font=('Calibry', 16), spacing1=2, spacing3=2)
+        self.bHistoryDec.grid(row=3, column=1, padx=8, pady=0, sticky="nsew")
+        # Hexadecimal History
+        self.bHistoryHex = ctk.CTkTextbox(master=self, font=('Calibry', 16), spacing1=2, spacing3=2)
+        self.bHistoryHex.grid(row=3, column=2, padx=8, pady=0, sticky="nsew")
+        # Dummy Label for Spacer at bottom
+        self.lSpacer = ctk.CTkLabel(master=self, text='', height=10, font=('Calibry', 6), anchor=ctk.W)
+        self.lSpacer.grid(row=4, column=0, columnspan=2, padx=8, pady=4, sticky="nsew")
+        # TODO: Synced scroll of the three results window
 
     def start(self):
-        self.app.mainloop()
+        self.mainloop()
 
     def onChangeInput(self, keypressed):
-        if keypressed.keysym=='Escape':
-            exit(0)
-        elif keypressed.keysym=='Return':
+        if keypressed.keysym == 'Return':
+            self.bExpressionHistory.tag_config('justified', justify=ctk.RIGHT)
+            self.bExpressionHistory.insert(0.0, self.bInput.get() + "\n", 'justified') 
+            self.bHistoryDec.tag_config('justified', justify=ctk.RIGHT)
+            self.bHistoryDec.insert(0.0, self.bResultDec.get() + "\n", 'justified') 
+            self.bHistoryHex.tag_config('justified', justify=ctk.RIGHT)
+            self.bHistoryHex.insert(0.0, self.bResultHex.get() + "\n", 'justified') 
             self.bInput.delete(0, 'end')
         else:
             self.updateResults()
