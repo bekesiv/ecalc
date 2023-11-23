2ad3ef6401227eeb25f41314d3e82aae63f47a7c
Viktor Bekesi <viktor.bekesi@outlook.com>
2023-11-23 22:14:02 +0100

    Progressing

--- ecalc/calculator.py
+++ ecalc/calculator.py
@@ -1,36 +1,68 @@
 #! /usr/bin/env python
 
 import customtkinter as ctk
-
+import ast
+from math import *
+import re
 
 class Calculator(object):
     def __init__(self):
-        ctk.set_appearance_mode("Dark")
-        ctk.set_default_color_theme("blue")
+        ctk.set_appearance_mode('Dark')
+        ctk.set_default_color_theme('blue')
         self.app = ctk.CTk()
-        self.app.title("eCalc")
-        self.app.geometry("800x400")
+        self.app.title('eCalc')
+        self.app.geometry('800x400')
         self.app.resizable(False, False)
-        # bInput = ctk.CTkEntry(master=app, width=800, font=('Calibry', 20), placeholder_text='Type Expression Here', state=ctk.DISABLED)
-        self.bInput = ctk.CTkEntry(master=self.app, width=800, font=('Calibry', 20), placeholder_text='Type Expression Here', justify="right")
-        self.bInput.bind('<KeyRelease>', self.evaluateExpression)
-        self.bInput.pack(pady=20, padx=20)
-        self.bInput.after(10, self.bInput.focus_set)
-        self.bInput.focus_set()
-        self.bResultDec = ctk.CTkTextbox(master=self.app, font=('Calibry', 20))
-        # bInput.place(relx=0.1, rely=0.2, anchor=ctk.CENTER)
-        self.bResultDec.pack(pady=30, padx=20)
-        
 
-    def start(self):        
+        self.bInput = ctk.CTkEntry(master=self.app, width=800, font=('Calibry', 20), placeholder_text='Type Expression Here', justify='right')
+        self.bInput.bind('<KeyRelease>', self.onChangeInput)
+        self.bInput.pack(pady=20, padx=20)
+        self.bInput.after(20, self.bInput.focus_set)
+        self.bInput.focus_set()
+
+        self.lResultDec = ctk.CTkLabel(master=self.app, text='Result', font=('Calibry', 16))
+        self.lResultDec.pack(pady=10, padx=20)
+
+        self.bResultDec = ctk.CTkEntry(master=self.app, width=400, font=('Calibry', 20), state=ctk.DISABLED)
+        self.bResultDec.bind('<Button>', self.onClickResultDec)
+        self.bResultDec.pack(pady=10, padx=20)
+
+        self.lResultHex = ctk.CTkLabel(master=self.app, text='Hexadecimal', font=('Calibry', 16))
+        self.lResultHex.pack(pady=10, padx=20)
+
+        self.bResultHex = ctk.CTkEntry(master=self.app, width=400, font=('Calibry', 20), state=ctk.DISABLED)
+        self.bResultHex.pack(pady=10, padx=20)
+
+    def start(self):
         self.app.mainloop()
 
-
-    def evaluateExpression(self, keypressed):
-        self.bResultDec.delete('1.0', ctk.END)
+    def onChangeInput(self, keypressed):
         if keypressed.keysym=='Escape':
             exit(0)
         elif keypressed.keysym=='Return':
             self.bInput.delete(0, 'end')
         else:
-            self.bResultDec.insert(ctk.END, self.bInput.get())
+            self.updateResults()
+
+    def onClickResultDec(self, dummy):
+        Calculator.clearDisabledEntry(self.bResultDec)        
+
+    @classmethod
+    def updateDisabledEntry(cls, entry, text):
+        entry.configure(state=ctk.NORMAL)
+        entry.delete(0, 'end')
+        entry.insert(ctk.END, text)
+        entry.configure(state=ctk.DISABLED)
+
+    def updateResults(self):
+            formula = self.bInput.get().replace('^', '**')
+            try:
+                result = eval(compile(ast.parse(formula, mode='eval'), filename='', mode='eval'))
+            except:
+                result = 'error'
+            Calculator.updateDisabledEntry(self.bResultDec, result)
+            try:
+                Calculator.updateDisabledEntry(self.bResultHex, result if result == 'error' else hex(int(result)))
+            except:
+                Calculator.updateDisabledEntry(self.bResultHex, 'error')
+            
