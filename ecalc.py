b9b5f0178ea8f404b2792323fb60f303ebcdf3cb
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-03-02 19:24:11 +0100

    Added Typing hints

--- ecalc.py
+++ ecalc.py
@@ -4,24 +4,25 @@ import re
 import ast
 from math import radians, sin, cos, tan
 import customtkinter as ctk # type: ignore
+from typing import Any, Optional, Union
 if os.name == 'posix':
     from PIL import Image, ImageTk
 
 APP_TITLE = 'eCalc'
-APP_VERSION = '1.2.3'
+APP_VERSION = '1.2.4'
 CONFIGURATION_FILENAME = f'{os.path.expanduser("~")}/ecalc.conf'
 DEFAULT_POSITION = '500x164+300+600'
 ORIGINAL_COLOR = ('#979DA2', '#565B5E')
-DEGREES='Degrees'
-RADIANS='Radians'
-DECIMAL='Decimal'
-HEXADECIMAL='Hexadecimal'
-BINARY='Binary'
+DEGREES = 'Degrees'
+RADIANS = 'Radians'
+DECIMAL = 'Decimal'
+HEXADECIMAL = 'Hexadecimal'
+BINARY = 'Binary'
 
 class Input(ctk.CTkComboBox):
-    def __init__(self, master):
+    def __init__(self, master: Any) -> None:
         super().__init__(master)
-        self.values = []
+        self.values: list[str] = []
         self.configure(values=self.values, command=self.onDropdown, fg_color='#222222',
                        font=('Calibry', 16), dropdown_font=('Calibry', 16),
                        justify='right', hover=True)
@@ -32,14 +33,14 @@ class Input(ctk.CTkComboBox):
         self.clear_flag = False
         self.after(50, self.focus_set)
 
-    def onDropdown(self, choice):
+    def onDropdown(self, choice: str) -> None:
         self.master.onChangeInput()
 
-    def onKeyPress(self, keypressed):
+    def onKeyPress(self, keypressed: Any) -> None:
         if keypressed.keysym not in ('Return', 'KP_Enter') and self.clear_flag:
             self.set('')
 
-    def onKeyRelease(self, keypressed):
+    def onKeyRelease(self, keypressed: Any) -> None:
         if keypressed.keysym in ('Return', 'KP_Enter'):
             self.master.onEnter()
             self.clear_flag = True
@@ -47,19 +48,19 @@ class Input(ctk.CTkComboBox):
         else:
             self.master.onChangeInput()
 
-    def addHistory(self):
+    def addHistory(self) -> None:
         self.values.insert(0, self.get())
         self.configure(values=self.values)
 
-    def flash(self, duration=100):
+    def flash(self, duration: int = 100) -> None:
         self.configure(border_color='lightgrey', button_color='lightgrey')
         self.after(duration, lambda: self.configure(border_color=ORIGINAL_COLOR, button_color=ORIGINAL_COLOR))
 
 
 class Result(ctk.CTkComboBox):
-    def __init__(self, master, base, row):
+    def __init__(self, master: Any, base: str, row: int) -> None:
         super().__init__(master)
-        self.values = []
+        self.values: list[str] = []
         self.configure(values=self.values, command=self.onDropdown, #height=32,
                        font=('Calibry', 16), dropdown_font=('Calibry', 16), justify='right',
                        hover=True)
@@ -72,13 +73,13 @@ class Result(ctk.CTkComboBox):
         label = ctk.CTkLabel(master=master, text=base, font=('Calibry', 16), anchor=ctk.W)
         label.grid(row=row, column=0, padx=8, pady=4, sticky="ew")
 
-    def onKeyEvent(self, keypressed):
+    def onKeyEvent(self, keypressed: Any) -> None:
         self.master.onChangeInput()
 
-    def onDropdown(self, choice):
+    def onDropdown(self, choice: str) -> None:
         self.master.onChangeInput(choice)
 
-    def write(self, content):
+    def write(self, content: str) -> None:
         if self.base != DECIMAL and content:
             try:
                 content = hex(int(content)) if self.base == HEXADECIMAL else bin(int(content))
@@ -86,19 +87,19 @@ class Result(ctk.CTkComboBox):
                 content = 'error'
         self.set(content)
 
-    def addHistory(self):
+    def addHistory(self) -> None:
         self.values.insert(0, self.get())
         self.configure(values=self.values)
 
-    def flash(self, duration=100):
+    def flash(self, duration: int = 100) -> None:
         self.configure(border_color='lightgrey', button_color='lightgrey')
         self.after(duration, lambda: self.configure(border_color=ORIGINAL_COLOR, button_color=ORIGINAL_COLOR))
 
-    def getValue(self, index):
+    def getValue(self, index: int) -> str:
         return self.values[index - 1]
 
 class Calculator(ctk.CTk):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(className=APP_TITLE)
         ctk.set_appearance_mode('Dark')
         ctk.set_default_color_theme('blue')
@@ -128,14 +129,14 @@ class Calculator(ctk.CTk):
         self.resultHex = Result(self, HEXADECIMAL, 2)
         self.resultBin = Result(self, BINARY, 3)
 
-    def start(self):
+    def start(self) -> None:
         self.mainloop()
 
-    def saveWindowPosition(self, event):
+    def saveWindowPosition(self, event: Any) -> None:
         with open(CONFIGURATION_FILENAME, "w", encoding="utf-8") as conf:
             conf.write(self.geometry())
 
-    def setAppIcon(self):
+    def setAppIcon(self) -> None:
         icon_path = os.path.join(os.path.dirname(__file__), '_internal', 'Wineass-Ios7-Redesign-Calculator')
         if os.name == 'nt':
             self.after(201, lambda: self.iconbitmap(icon_path + '.ico'))
@@ -144,40 +145,40 @@ class Calculator(ctk.CTk):
             photo = ImageTk.PhotoImage(im)
             self.iconphoto(True, photo)
 
-    def setGeometry(self):
+    def setGeometry(self) -> None:
         try:
             with open(CONFIGURATION_FILENAME, "r", encoding="utf-8") as conf:
                 self.geometry(conf.readlines()[0])
         except FileNotFoundError:
             self.geometry(DEFAULT_POSITION)
 
-    def onEnter(self):
+    def onEnter(self) -> None:
         self.input.addHistory()
         self.resultDec.addHistory()
         self.resultHex.addHistory()
         self.resultBin.addHistory()
 
-    def onUpdateSwitch(self):
+    def onUpdateSwitch(self) -> None:
         self.switchDR.configure(text=self.switchDRValue.get())
         self.onChangeInput()
 
-    def onChangeInput(self, value=None):
+    def onChangeInput(self, value: Optional[str] = None) -> None:
         self.input.clear_flag = False
         if value:
             self.input.set(value)
         result = self.calculate(self.input.get())
-        self.resultDec.write((result))
-        self.resultHex.write((result))
-        self.resultBin.write((result))
+        self.resultDec.write(result)
+        self.resultHex.write(result)
+        self.resultBin.write(result)
         self.input.focus_set()
 
-    def flashWidgets(self):
+    def flashWidgets(self) -> None:
         self.input.flash()
         self.resultDec.flash()
         self.resultHex.flash()
         self.resultBin.flash()
 
-    def calculate(self, formula):
+    def calculate(self, formula: str) -> Union[str, float]:
         retVal = formula.replace('^', '**').replace(',', '.')
         if formula:
             if self.switchDRValue.get() == DEGREES:
